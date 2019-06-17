eefd9e2cc0760795b39c35451f950c33258285c6
joeyklee <jsphknglee@gmail.com>
2019-06-17 15:35:31 -0400

    added updates

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 const xProp = 'Population (CDP)';
 const yProp = 'Scope-1 GHG emissions [tCO2 or tCO2-eq]'
+let status;
+let interact;
+let model;
+let result;
+let submitBtn;
+let tensorData;
 
-
-async function getData(){
+async function getData() {
     let output = await fetch('input/D_FINAL-Selected.json')
     output = await output.json()
 
     // clean up
     output.data = output.data.filter(item => {
-        if(item[xProp] !== "" && item[xProp] !== null && item[xProp] !== undefined && item[xProp] > 1 &&
-        item[yProp] !== "" && item[yProp] !== null && item[yProp] !== undefined && item[yProp] > 1 ){
-            console.log(item[xProp], item[yProp])
+        if (item[xProp] !== "" && item[xProp] !== null && item[xProp] !== undefined && item[xProp] > 1 &&
+            item[yProp] !== "" && item[yProp] !== null && item[yProp] !== undefined && item[yProp] > 1) {
             item[xProp] = Math.log10(item[xProp])
             item[yProp] = Math.log10(item[yProp])
             // item[xProp] = parseFloat(item[xProp])
@@ -22,40 +26,47 @@ async function getData(){
     return output
 }
 
-function visualizeVals(data){
+function visualizeVals(data) {
     const values = data.data.map(d => ({
         x: d[xProp],
         y: d[yProp],
-      }));
-    
-      console.log(values)
-      tfvis.render.scatterplot(
-        {name: `${xProp} vs ${yProp}`},
-        {values}, 
-        {
-          xLabel: xProp,
-          yLabel: yProp,
-          height: 400
-        }
-      );
+    }));
+
+    console.log(values)
+    tfvis.render.scatterplot({
+        name: `${xProp} vs ${yProp}`
+    }, {
+        values
+    }, {
+        xLabel: xProp,
+        yLabel: yProp,
+        height: 400
+    });
 }
 
 
 function createModel() {
     // Create a sequential model
-    const model = tf.sequential(); 
-    
+    const model = tf.sequential();
+
     // Add a single hidden layer
-    model.add(tf.layers.dense({inputShape: [1], units: 1, useBias: true}));
-    
+    model.add(tf.layers.dense({
+        inputShape: [1],
+        units: 1,
+        useBias: true
+    }));
+
     // Add an output layer
-    model.add(tf.layers.dense({units: 1, useBias: true}));
-  
+    model.add(tf.layers.dense({
+        units: 1,
+        useBias: true
+    }));
+
     return model;
 }
 
-function convertToTensor(data){
-    return tf.tidy( () => {
+function convertToTensor(data) {
+    return tf.tidy(() => {
         // Step 1. Shuffle the data    
         tf.util.shuffle(data);
 
@@ -68,7 +79,7 @@ function convertToTensor(data){
 
         //Step 3. Normalize the data to the range 0 - 1 using min-max scaling
         const inputMax = inputTensor.max();
-        const inputMin = inputTensor.min();  
+        const inputMin = inputTensor.min();
         const labelMax = labelTensor.max();
         const labelMin = labelTensor.min();
 
@@ -83,19 +94,19 @@ function convertToTensor(data){
             inputMin,
             labelMax,
             labelMin,
-          }
+        }
 
     })
 }
 
 
 
-async function trainModel(model, inputs, labels){
+async function trainModel(model, inputs, labels) {
 
     model.compile({
         optimizer: tf.train.adam(),
         loss: tf.losses.meanSquaredError,
-        metrics:['mse']
+        metrics: ['mse']
     })
 
     const batchSize = 28;
@@ -104,70 +115,136 @@ async function trainModel(model, inputs, labels){
     return await model.fit(inputs, labels, {
         batchSize,
         epochs,
-        shuffle:true,
-        callbacks: tfvis.show.fitCallbacks(
-            {name:'training performance'},
-            ['loss', 'mse'],
-            {height: 300, callbacks:['onEpochEnd']}
+        shuffle: true,
+        callbacks: tfvis.show.fitCallbacks({
+                name: 'training performance'
+            },
+            ['loss', 'mse'], {
+                height: 300,
+                callbacks: ['onEpochEnd']
+            }
         )
     })
 
 }
 
 
-function testModel(model, inputData, normalizationData){
-    const {inputMax, inputMin, labelMin, labelMax} = normalizationData;
+function predict(_val) {
+    let convertedInput = tf.tensor([Math.log10(_val)]);
+    // let convertedInput = _val;
 
-    const [xs, preds] = tf.tidy( () => {
+    const {
+        inputMax,
+        inputMin,
+        labelMin,
+        labelMax
+    } = tensorData;
+
+    const normalizedInputs = convertedInput.sub(inputMin).div(inputMax.sub(inputMin));
+
+    normalizedInputs.print()
+    const [preds] = tf.tidy(() => {
+        const preds = model.predict(normalizedInputs)
+
+        const unNormPreds = preds
+            .mul(labelMax.sub(labelMin))
+            .add(labelMin);
+
+        return [unNormPreds.dataSync()]
+    })
+
+    const predictedPoints = Array.from(preds).map((val, i) => {
+        return {
+            x: convertedInput.dataSync()[0],
+            y: val
+        }
+    });
+
+
+    return predictedPoints[0]
+}
+
+
+function testModel(model, inputData, normalizationData) {
+    const {
+        inputMax,
+        inputMin,
+        labelMin,
+        labelMax
+    } = normalizationData;
+
+    const [xs, preds] = tf.tidy(() => {
         const xs = tf.linspace(0, 1, 100);
         const preds = model.predict(xs.reshape([100, 1]))
 
         const unNormXs = xs
-                        .mul(inputMax.sub(inputMin))
-                        .add(inputMin);
-    
+            .mul(inputMax.sub(inputMin))
+            .add(inputMin);
+
         const unNormPreds = preds
-                        .mul(labelMax.sub(labelMin))
-                        .add(labelMin);
+            .mul(labelMax.sub(labelMin))
+            .add(labelMin);
 
         return [unNormXs.dataSync(), unNormPreds.dataSync()]
     })
 
     const predictedPoints = Array.from(xs).map((val, i) => {
-        return {x: val, y: preds[i]}
-      });
-      
-      const originalPoints = inputData.map(d => ({
-        x: d[xProp], y: d[yProp],
-      }));
-      
-      
-      tfvis.render.scatterplot(
-        {name: `model predictions vs original data`}, 
-        {values: [originalPoints, predictedPoints], series: ['original', 'predicted']}, 
-        {
-          xLabel: xProp,
-          yLabel: yProp,
-          height: 400
+        return {
+            x: val,
+            y: preds[i]
         }
-      );
+    });
+
+    const originalPoints = inputData.map(d => ({
+        x: d[xProp],
+        y: d[yProp],
+    }));
+
+    tfvis.render.scatterplot({
+        name: `model predictions vs original data`
+    }, {
+        values: [originalPoints, predictedPoints],
+        series: ['original', 'predicted']
+    }, {
+        xLabel: xProp,
+        yLabel: yProp,
+        height: 400
+    });
 
 }
 
 
-let model;
+function handleSubmit() {
+    result = document.querySelector("#result");
+    const input = document.querySelector('#population-input').value
 
-async function make(){
+    const prediction = predict(input);
+    result.innerHTML =  Math.pow(10, prediction.y)
+}
+
+
+
+
+async function make() {
+    // dom stuff
+    status = document.querySelector("#status");
+    interact = document.querySelector("#interact");
+    submitBtn = document.querySelector("#submit-btn")
+    submitBtn.addEventListener('click', handleSubmit);
+
     const data = await getData();
-    
+
 
     visualizeVals(data)
 
     // Create the model
-    model = createModel();  
+    model = createModel();
 
-    const tensorData = convertToTensor(data.data);
-    const {inputs, labels} = tensorData;
+    tensorData = convertToTensor(data.data);
+    const {
+        inputs,
+        labels
+    } = tensorData;
 
     await trainModel(model, inputs, labels);
 
@@ -175,12 +252,15 @@ async function make(){
 
     testModel(model, data.data, tensorData);
 
-    // await model.save('downloads://my-model-co2-pop.json');
+    // await model.save('downloads://co2Net.json');
+    tfvis.show.modelSummary({
+        name: 'Model Summary'
+    }, model);
 
-    tfvis.show.modelSummary({name: 'Model Summary'}, model);
+    status = document.querySelector("#status");
 
-    // model.save();
-    
+    status.innerHTML = "done training! if your model didn't achieve a good fit, refresh and try again!";
+    interact.classList.toggle('hidden');
 
 }
 make()
